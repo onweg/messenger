313d2531a7d68f1ffc695b174c280fa3a311cf62
onweg <155016609+onweg@users.noreply.github.com>
2024-04-09 15:55:28 +0300

    add files

---
+++ Message/Message/App/MessageApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessageApp.swift
+//  Message
+//
+//  Created by Пользователь on 03.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct MessageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
